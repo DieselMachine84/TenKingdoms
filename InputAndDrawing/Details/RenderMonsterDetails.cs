cbcc08c8972406a8c17f98d44661e88ae5fc7337
DieselMachine <dieselmachine84@gmail.com>
2026-02-28 09:25:54 +0300

    Draw monster details

--- InputAndDrawing/Details/RenderMonsterDetails.cs
+++ InputAndDrawing/Details/RenderMonsterDetails.cs
@@ -4,11 +4,17 @@ public partial class Renderer
 {
     public void DrawMonsterDetails(UnitMonster monster)
     {
-        //
+        DrawUnitPanel(DetailsX1 + 2, DetailsY1 + 48);
+        
+        UnitInfo unitInfo = UnitRes[monster.UnitType];
+        Graphics.DrawBitmap(unitInfo.GetLargeIconTexture(Graphics, Unit.RANK_SOLDIER), DetailsX1 + 12, DetailsY1 + 56,
+            unitInfo.SoldierIconWidth * 2, unitInfo.SoldierIconHeight * 2);
+
+        PutTextCenter(FontSan, monster.GetUnitName(false), DetailsX1 + 10 + unitInfo.SoldierIconWidth * 2, DetailsY1 + 36 + unitInfo.SoldierIconHeight,
+            DetailsX2 - 4, DetailsY1 + 36 + unitInfo.SoldierIconHeight * 2);
     }
 
     public void HandleMonsterDetailsInput(UnitMonster monster)
     {
-        //
     }
 }
